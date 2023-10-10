16ad27d3ec009f9ddf824cd83b18c40154929368
PavelV <pascha27_05@mail.ru>
2023-10-10 17:54:13 +0700

    Добавьте файлы проекта.

---
+++ Vorobyev_Yadikcin_PR3_wpf/Pages/Auto.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vorobyev_Yadikcin_PR3_wpf.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autho.xaml
+    /// </summary>
+    public partial class Auto : Page
+    {
+        public Auto()
+        {
+            InitializeComponent();
+        }
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new Client(null)); //Переход на страницу неавторизованного пользователя
+        }
+        private void btnEnterGuests_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Client(null)); //Переход на страницу неавторизованного пользователя
+        }     
+    }
+}
